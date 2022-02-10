230d8b0713822640663cc565edd760892dae7484
Cole Tucker <cjtucker321@gmail.com>
2022-02-09 21:13:10 -0500

    weird package issue

--- master-control-node/master-control-node/master-control.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from master-control-node.')
-
-
-if __name__ == '__main__':
-    main()
--- master-control-node/setup.py
+++ master-control-node/setup.py
@@ -14,13 +14,12 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='coalman321',
-    maintainer_email='coalman321@todo.todo',
+    maintainer_email='cjtucker321@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'master-control = master-control-node.master-control:main'
         ],
     },
 )
